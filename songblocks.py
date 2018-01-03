bb98b7354c1274fc0cdd1f313df87c72958ac2ba
Daniel Quinlan <daniel@chaosengine.net>
2018-01-02 19:08:15 -0800

    refactor to add logging, testing and later an alt to Sonos

--- songblocks.py
+++ songblocks.py
@@ -3,11 +3,16 @@
 import ConfigParser
 import logging
 import logging.config
-import nfc
-import soco
 import time
 
-class NFCPollForTagID(object):
+# TODO add class/method docstrings
+
+class Poller(object):
+  def run(self, tagPresent=None, tagRemoved=None):
+      pass
+
+
+class NFCPollForTagID(Poller):
   poll_last_tag = None
 
   def __init__(self, device_path, poll_interval_secs=0.5):
@@ -45,24 +50,32 @@ class NFCPollForTagID(object):
             tagPresent(tag)
     
       time.sleep(self.poll_interval_secs)
- 
 
-class NFCSonosController(object):
+
+class MockNFCPollForTagID(NFCPollForTagID):
+  def poll_for_tag(self):
+    # TODO add a way to trigger mock tags
+    return '2b1f76dd'
+
+
+class PlayerController(object):
+  def action_foobar(self, actionConfig):
+      pass
+
+  def stop(self):
+      pass
+
+
+class SonosController(PlayerController):
   _player = None
 
-  def __init__(self, configFile):
-    self._configFile = configFile
-    self.config = ConfigParser.SafeConfigParser()
-    self.config.read(configFile)
-
-  def connect(self, player_name=None):
-    if player_name is None:
-      player_name = self.config.get("Config", "player_name")
+  def __init__(self, player_name):
     self.player_name = player_name
     self._player = soco.discovery.by_name(player_name)
-    return self._player
+    if self._player is None:
+        raise "Sonos player %s not found" % player_name
 
-  def sonosPlayUri(self, uri):
+  def playUri(self, uri):
     logging.info("  playing URI %s" % uri)
     try:
       self._player.clear_queue()
@@ -71,25 +84,58 @@ class NFCSonosController(object):
     except soco.SoCoException, e:
       logging.exception(e)
 
-  def sonosStop(self):
+  def playPlaylist(self, playlistName):
+    logging.info("  playing playlist '%s'" % playlistName)
+    try:
+      playlist = self._player.get_sonos_playlist_by_attr('title', playlistName)
+      self.playUri(playlist.get_uri())
+    except ValueError:
+      logging.error("  Sonos playlist %s not found" % playlistName)
+    except soco.SoCoException, e:
+      logging.exception(e)
+
+  def stop(self):
     logging.info("stop playing")
     try:
       self._player.stop()
     except soco.SoCoException, e:
       logging.exception(e)
 
-  def action_sonos_uri(self, tagConfig):
-    if tagConfig.has_key(tagConfig['uri']):
-      logging.info("  playing %s" % tagConfig['name'])
-      self.sonosPlayUri(tagConfig['uri'])
+  def action_sonos_uri(self, actionConfig):
+    if actionConfig.has_key('uri'):
+      logging.info("  playing %s" % actionConfig.get('name', actionConfig['uri']))
+      self.playUri(actionConfig['uri'])
+    else:
+      logging.error("  uri not defined")
 
-  def action_sonos_playlist(self, tagConfig):
-    logging.info("  playing playlist '%s'" % tagConfig['playlist'])
-    try:
-      playlist = self._player.get_sonos_playlist_by_attr('title', tagConfig['playlist'])
-      self.sonosPlayUri(playlist.get_uri())
-    except ValueError:
-      logging.info("  Sonos playlist %s not found" % tagConfig['playlist'])
+  def action_sonos_playlist(self, actionConfig):
+    if actionConfig.has_key('playlist'):
+      logging.info("  playing %s" % actionConfig['playlist'])
+      self.playPlaylist(actionConfig['playlist'])
+    else:
+      logging.error("  playlist not defined")
+
+
+class MockSonosController(SonosController):
+    def __init__(self, player_name):
+        self.player_name = player_name
+
+    def playUri(self, uri):
+        logging.debug("play URI %s" % uri)
+
+    def playPlaylist(self, playlistName):
+        logging.debug("play playlist %s" % playlistName)
+
+    def stop(self):
+        logging.debug("stop playing")
+
+
+class SongBlocks(object):
+
+  def __init__(self, config, poller, player):
+    self.config = config
+    self.poller = poller
+    self.player = player
 
   def tagPresent(self, tag):
     logging.info("play song for tag %s" % tag)
@@ -97,31 +143,42 @@ class NFCSonosController(object):
     if self.config.has_section(tagSection):
       tagConfig = dict(self.config.items(tagSection))
       actionName = 'action_%s' % tagConfig['action'].lower().replace(' ', '_')
-      action = getattr(self, actionName, None)
+      action = getattr(self.player, actionName, None)
       if action is not None:
         action(tagConfig)
       else:
-        logging.warning("  unknown action %s for [%s] in %s" % (actionName, tagSection, self._configFile))
+        logging.warning("  unknown action %s for [%s] in config file" % (actionName, tagSection))
     else:
-      logging.warning("  [%s] not found in %s" % (tagSection, self._configFile))
+      logging.warning("  [%s] not found in config file" % tagSection)
 
   def tagRemoved(self):
-    self.sonosStop()
+    self.player.stop()
 
-  def run(self, device_path=None):
-    if device_path is None:
-      device_path = self.config.get("Config", "nfc_device_path")
-    poller = NFCPollForTagID(device_path)
-    poller.run(tagPresent=self.tagPresent, tagRemoved=self.tagRemoved)
+  def run(self):
+    self.poller.run(tagPresent=self.tagPresent, tagRemoved=self.tagRemoved)
 
 
 # main
 if __name__ == "__main__":
-  logging.config.fileConfig('songblocks.ini')
+  # TODO add CLI args
+  configFile = 'songblocks.ini'
+  logging.config.fileConfig(configFile)
 
-  controller = NFCSonosController(configFile='songblocks.ini')
-  if controller.connect() is not None:
-    controller.run()
+  config = ConfigParser.SafeConfigParser()
+  config.read(configFile)
+  device_path = config.get("Config", "nfc_device_path")
+  player_name = config.get("Config", "player_name")
+
+  if config.get('Config', 'testing').lower() == "true":
+    # TODO do this with test cases instead
+    poller = MockNFCPollForTagID(player_name)
+    player = MockSonosController(device_path)
   else:
-    logging.error("Sonos player %s not found" % controller.player_name)
+    import nfc
+    import soco
+    poller = NFCPollForTagID(device_path)
+    player = SonosController(player_name)
+
+  controller = SongBlocks(config, poller, player)
+  controller.run()
 
