5a16e118e61a00bb2a76c081105b3ab96f81fd73
Daniel Quinlan <daniel@chaosengine.net>
2018-02-19 23:24:51 -0800

    oops, missed the partial poller refactor.  reverted

--- songblocks.py
+++ songblocks.py
@@ -14,6 +14,56 @@ class SongblocksException(Exception):
   pass
 
 
+class Poller(object):
+  """ABC for a polling device to provide 'tag' UUIDs."""
+
+  def __init__(self, device_path):
+      pass
+
+  def run(self, tagPresent=None, tagRemoved=None):
+      pass
+
+
+class NFCPoller(Poller):
+  poll_last_tag = None
+
+  def __init__(self, device_path, poll_interval_secs=0.5):
+    self.poll_interval_secs = poll_interval_secs
+    self.device_path = device_path
+
+  def poll_for_tag(self):
+    """Poll device at device_path for a tag, return the UID if found, None if not. """
+    with nfc.clf.ContactlessFrontend(self.device_path) as clf:
+        target = clf.sense(nfc.clf.RemoteTarget("106A"))
+        if target is not None:
+            tag = nfc.tag.activate(clf, target)
+            return str(tag.identifier).encode('hex')
+        else:
+            return None
+
+  def run(self, tagPresent=None, tagRemoved=None):
+    while True:
+      tag = self.poll_for_tag()
+      if tag is None:
+        logging.debug("no tag detected")
+        if self.poll_last_tag is not None:
+          logging.debug("  tag %s was present last time" % self.poll_last_tag)
+          self.poll_last_tag = None
+          if tagRemoved is not None:
+            tagRemoved()
+      else:
+        logging.debug("tag detected: %s" % tag)
+        if tag == self.poll_last_tag:
+          logging.debug("  same tag as last time")
+        else:
+          logging.debug("  new tag")
+          self.poll_last_tag = tag
+          if tagPresent is not None:
+            tagPresent(tag)
+    
+      time.sleep(self.poll_interval_secs)
+
+
 class MockNFCPoller(NFCPoller):
   def poll_for_tag(self):
     # TODO add a way to trigger mock tags
