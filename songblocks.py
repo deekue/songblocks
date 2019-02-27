380e4d2513be1c57345f88e539cb89a280763143
Daniel Quinlan <daniel@chaosengine.net>
2019-02-26 21:31:20 -0800

    make Mocks configurable rather than a fallback

--- songblocks.py
+++ songblocks.py
@@ -41,31 +41,32 @@ class SongBlocks(object):
     self.poller.run(tagPresent=self.tagPresent, tagRemoved=self.tagRemoved)
 
 
-def importPoller(device_path):
-  try:
+def importPoller(poller_type, device_path):
+  if poller_type == "Nfctag":
     from poller.nfctag import NFCPoller
     poller = NFCPoller(device_path)
     logging.debug("chose NFCPoller")
-  except ImportError:
+  elif poller_type == "Mock":
     from poller.mock import MockPoller
     poller = MockPoller(device_path)
     logging.debug("chose MockPoller")
+  else:
+    raise SongblocksException("Unknown poller_type %s" % poller_type)
   return poller
 
 def importPlayer(player_type, player_name):
-  try:
-    if player_type == "Sonos":
-      from player.sonos import SonosController
-      player = SonosController(player_name)
-    elif player_type == "Chromecast":
-      from player.chromecast import ChromecastController
-      player = ChromecastController(player_name)
-    else:
-      raise SongblocksException("Unknown player_type %s" % player_type)
-  except ImportError:
+  if player_type == "Sonos":
+    from player.sonos import SonosController
+    player = SonosController(player_name)
+  elif player_type == "Chromecast":
+    from player.chromecast import ChromecastController
+    player = ChromecastController(player_name)
+  elif player_type == "Mock":
     from player.mock import MockController
     player = MockController(player_name)
     logging.debug("chose MockController")
+  else:
+    raise SongblocksException("Unknown player_type %s" % player_type)
   return player
 
 def main(argv=None):
@@ -81,15 +82,16 @@ def main(argv=None):
 
   config = ConfigParser.SafeConfigParser()
   config.read(options.config_file)
-  device_path = config.get("Config", "nfc_device_path")
+  poller_type = config.get("Config", "poller_type").lower().capitalize()
+  device_path = config.get("Config", "poller_device_path")
   player_name = config.get("Config", "player_name")
   player_type = config.get("Config", "player_type").lower().capitalize()
 
-  poller = importPoller(device_path)
+  poller = importPoller(poller_type, device_path)
   player = importPlayer(player_type, player_name)
 
   controller = SongBlocks(config, poller, player)
   controller.run()
 
 if __name__ == "__main__":
-  sys.exit(main())
+  sys.exit(main(sys.argv))
